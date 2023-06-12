3fedc354fae6033ee4aa22740657ac077076a730
JackRio <sanvyawahare@gmail.com>
2023-06-12 22:59:08 +0200

    Adding pytorch lightning and refactoring codebase

--- dataloader.py
+++ dataloader.py
@@ -2,13 +2,14 @@ import os
 
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 
 
 class BoneAgeDataset(Dataset):
     def __init__(self, annotations_file, transform=None):
-        self.img_labels = pd.read_csv(annotations_file)
+        self.img_labels = annotations_file
         self.transform = transform
 
     def __len__(self):
@@ -24,28 +25,28 @@ class BoneAgeDataset(Dataset):
         boneage = self.img_labels.boneage.iloc[idx]
         if self.transform:
             augmented = self.transform(image=image)
-            image = augmented["image"].transpose(2, 0, 1)
-        return {"image": image, "gender": gender, "boneage": boneage}
+            image = augmented["image"]
+        return {"image": image, "gender": gender[..., np.newaxis], "boneage": boneage[..., np.newaxis]}
 
 
 if __name__ == "__main__":
     import albumentations as A
 
-    transform = A.Compose([
+    augmentations = A.Compose([
         A.Resize(width=450, height=450),
         A.CenterCrop(width=350, height=350),
         A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=0.5)
     ])
 
-    bad_train = BoneAgeDataset(annotations_file=os.path.join('data', 'rsna-bone-age', 'training', 'train_df.csv'),
-                               transform=transform)
+    bad_train = BoneAgeDataset(
+        annotations_file=pd.read_csv(os.path.join('data', 'rsna-bone-age', 'training', 'train_df.csv')),
+        transform=augmentations)
 
     dataloader = DataLoader(bad_train, batch_size=16, shuffle=True, num_workers=0)
     print(len(dataloader))
     for i_batch, sample_batched in enumerate(dataloader):
         # plot an image from the batch
         print(sample_batched['image'][0].size())
-        plt.imshow(sample_batched['image'][0].permute(1, 2, 0))
+        plt.imshow(sample_batched['image'][0])
         plt.show()
         break
-
--- main.py
+++ main.py
@@ -1,83 +1,88 @@
 import os
 
 import albumentations as A
+import pandas as pd
+import pytorch_lightning as L
 import torch
 import torch.nn as nn
-import torchvision.models as models
+from albumentations.pytorch import ToTensorV2
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
 from torch.utils.data import DataLoader
 
+from config import constants as C
 from dataloader import BoneAgeDataset
+from models.swin_b import SwinB
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print("Using {} device".format(device))
-
 # Define the transformations
 transform = A.Compose([
     A.Resize(width=450, height=450),
-    A.CenterCrop(width=250, height=250),
+    A.CenterCrop(width=256, height=256),
+    A.Normalize(),
+    ToTensorV2(),
 ])
 
-num_epochs = 100
-batch_size = 32
-learning_rate = 0.005
+num_epochs = 50
+batch_size = 16
+learning_rate = 0.0005
 
-bad_train = BoneAgeDataset(annotations_file=os.path.join('data', 'rsna-bone-age', 'training', 'train_df.csv'),
-                           transform=transform)
+# Load the data
+raw_train_df = pd.read_csv(C.TRAIN_CSV)
+valid_df = pd.read_csv(C.VALID_CSV)
+
+bad_train = BoneAgeDataset(annotations_file=raw_train_df, transform=transform)
 train_dataloader = DataLoader(bad_train, batch_size=batch_size, shuffle=True, num_workers=0)
 
-bad_valid = BoneAgeDataset(annotations_file=os.path.join('data', 'rsna-bone-age', 'training', 'valid_df.csv'),
-                           transform=transform)
-valid_dataloader = DataLoader(bad_valid, batch_size=batch_size, shuffle=True, num_workers=0)
+bad_valid = BoneAgeDataset(annotations_file=valid_df, transform=transform)
+valid_dataloader = DataLoader(bad_valid, batch_size=batch_size, shuffle=False, num_workers=0)
 
-model = models.vgg16()
-model.classifier[-1] = nn.Linear(in_features=4096, out_features=1)
-model = model.to(device)
 
-# Loss and optimizer
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0.005)
+def train_model(**kwargs):
+    trainer = L.Trainer(
+        default_root_dir=os.path.join(C.CHECKPOINT_PATH, "SwinB"),
+        accelerator="auto",
+        devices=1,
+        max_epochs=50,
+        callbacks=[
+            ModelCheckpoint(save_weights_only=True, mode="min", monitor="val_loss"),
+            LearningRateMonitor("epoch"),
+        ]
+        # logger=L.loggers.TensorBoardLogger(save_dir=C.LOGS_PATH, name="swinb")
+    )
+    # trainer.logger._log_graph = True  # If True, we plot the computation graph in tensorboard
+    # trainer.logger._default_hp_metric = None  # Optional logging argument that we don't need
+    print(trainer.checkpoint_callback.best_model_path)
 
-# Train the model
-total_step = len(train_dataloader)
+    if os.path.isfile(C.PRETRAINED_FILENAME):
+        print("Found pretrained model at %s, loading..." % C.PRETRAINED_FILENAME)
+        # Automatically loads the model with the saved hyperparameters
+        if C.PRETRAINED_FILENAME.endswith("ckpt"):
+            model = SwinB.load_from_checkpoint(C.PRETRAINED_FILENAME)
+        else:
+            model = SwinB(**kwargs)
+            model.model.load_state_dict(torch.load(C.PRETRAINED_FILENAME))
+            model.model.head = nn.Linear(in_features=1024, out_features=1)
+        if is_training:
+            trainer.fit(model, train_dataloader, valid_dataloader)
+            model = SwinB.load_state_dict(trainer.checkpoint_callback.best_model_path)
+    else:
+        L.seed_everything(42)  # To be reproducable
+        model = SwinB(**kwargs)
+        trainer.fit(model, train_dataloader, valid_dataloader)
+        # Load best checkpoint after training
+        model = SwinB.load_state_dict(trainer.checkpoint_callback.best_model_path)
 
-for epoch in range(num_epochs):
-    for i, scans in enumerate(train_dataloader):
-        # Print the progress every 100 batches
-        if (i + 1) % 100 == 0:
-            print(f"\rEpoch {epoch + 1}/{num_epochs} [{i + 1}/{total_step}] ", end='')
+    # Test best model on validation and test set
+    val_result = trainer.test(model, dataloaders=valid_dataloader, verbose=False)
+    #     test_result = trainer.test(model, dataloaders=test_loader, verbose=False)
+    result = {
+        #         "test": test_result[0]["test_acc"],
+        "val": val_result[0]["test_acc"]
+    }
 
-        # Move tensors to the configured device
-        images = scans['image'].to(torch.float32).to(device)
-        labels = scans['boneage'].to(torch.float32).to(device)
-        labels = torch.unsqueeze(labels, dim=1)
+    return model, result
 
-        # Forward pass
-        outputs = model(images)
 
-        loss = torch.sqrt(criterion(outputs, labels))
-
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-    print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-          .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
-
-    # Validation
-    with torch.no_grad():
-        for scans in valid_dataloader:
-            images = scans['image'].to(torch.float32).to(device)
-            labels = scans['boneage'].to(torch.float32).to(device)
-            labels = torch.unsqueeze(labels, dim=1)
-
-            with torch.no_grad():
-                outputs = model(images)
-                val_loss = torch.sqrt(criterion(outputs, labels))
-            del images, labels, outputs
-
-        print('Accuracy of the network on the {} validation images: {} %'.format(len(valid_dataloader) * batch_size,
-                                                                                 val_loss))
-
-    if epoch % 10 == 0:
-        torch.save(model.state_dict(), 'output/model_vgg16_{}.ckpt'.format(epoch))
+is_training = True
+train_model(model_kwargs={}, lr=0.0001)
