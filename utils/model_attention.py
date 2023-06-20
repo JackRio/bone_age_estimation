4dc7b119c581b4f10fc0d28d468156a8f0cb6257
JackRio <sanvyawahare@gmail.com>
2023-06-20 11:15:41 +0200

    Adding model activation heatmap code

--- utils/model_attention.py
+++ utils/model_attention.py
@@ -1,44 +1,65 @@
-import albumentations as A
 import cv2
 import matplotlib.pyplot as plt
-import numpy as np
-import torch
-import yaml
 import pandas as pd
-from albumentations.pytorch import ToTensorV2
+import torchvision.transforms as transforms
+import tqdm
+from matplotlib.backends.backend_pdf import PdfPages
 from pytorch_grad_cam import GradCAM
 from pytorch_grad_cam.utils.image import show_cam_on_image
-from torch.utils.data import DataLoader
 
-from config import constants as C
-from dataloader import BoneAgeDataset
 from models.resnet import ResNet
 
-with open(C.MODEL_ATTENTION_CONFIG, "r") as f:
-    config = yaml.safe_load(f)
+resnet = ResNet.load_from_checkpoint('lightning_logs/mkmcrxik/checkpoints/epoch=14-step=4440.ckpt')
+target_layers = [resnet.model.layer4[-1]]
+resnet.model.eval()
 
-augmentations = A.Compose([
-    A.Resize(width=512, height=512),
-    A.Normalize(),
-    ToTensorV2()
-])
-dataset = BoneAgeDataset(pd.read_csv(config['annotation_file']), transform=augmentations)
-val_loader = DataLoader(dataset, batch_size=4, shuffle=True, num_workers=0)
-resnet = ResNet.load_from_checkpoint('lightning_logs/fmt8pml9/checkpoints/epoch=0-step=148.ckpt')
+validation_df = pd.read_csv("data/rsna-bone-age/training/valid_df.csv").head(300)
+ids = validation_df["id"].tolist()
 
-for i, scan in enumerate(val_loader):
-    input_tensor = scan['image']
+# Create a PDF file for saving plots
+pdf_path = "output/resnet_pretrained_validation.pdf"
+with PdfPages(pdf_path) as pdf:
+    for id in tqdm.tqdm(ids):
+        image = cv2.imread(validation_df[validation_df["id"] == id]["path"].tolist()[0])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # Define the transformations
+        transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize((512, 512)),
+            transforms.ToTensor()
+        ])
 
-    target_layers = [resnet.model.layer4[-1]]
+        # Apply the transformations to the image
+        tensor_image = transform(image).unsqueeze(0)
+        cuda_tensor = tensor_image.to(device='cuda')
+        output = resnet.model(cuda_tensor)
+        predicted = int(output.item())
+        actual = validation_df[validation_df["id"] == id]["boneage"].tolist()[0]
+        # Construct the CAM object once, and then re-use it on many images:
+        cam = GradCAM(model=resnet.model, target_layers=target_layers, use_cuda=True)
 
-    # Construct the CAM object once, and then re-use it on many images:
-    cam = GradCAM(model=resnet.model, target_layers=target_layers, use_cuda=True)
+        # You can also pass aug_smooth=True and eigen_smooth=True, to apply smoothing.
+        grayscale_cam = cam(input_tensor=tensor_image)
+        visualization = show_cam_on_image(tensor_image[0].permute(1, 2, 0).numpy(), grayscale_cam[0], use_rgb=True)
 
-    # You can also pass aug_smooth=True and eigen_smooth=True, to apply smoothing.
-    grayscale_cam = cam(input_tensor=input_tensor)
+        # Add colorbar for the heatmap
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
 
-    # In this example grayscale_cam has only one image in the batch:
-    grayscale_cam = grayscale_cam[0, :]
-    visualization = show_cam_on_image(input_tensor.numpy(), grayscale_cam, use_rgb=True)
+        # Plot the visualization with colorbar
+        img = ax1.imshow(visualization)
+        fig.colorbar(img, ax=ax1)
 
-print(visualization)
+        # Plot the tensor_image
+        ax2.imshow(tensor_image[0].permute(1, 2, 0).numpy())
+        ax2.axis('off')
+
+        # Add text with output and predicted values
+        text = f"Predicted: {predicted} | Actual: {actual}"
+        plt.text(10, 30, text, fontsize=12, color='white', backgroundcolor='black')
+        plt.title(f"ID: {id}")
+
+        # Save the plot to the PDF file
+        pdf.savefig()
+
+        # Close the plot to free memory
+        plt.close()
